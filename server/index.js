e10e9831f37903788d2aef4da75ef59b198b93d7
Lara <volynkinala@gmail.com>
2023-02-01 15:04:37 +0400

    feat: implement file upload

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import userRouter from './routes/userRoute.js';
+import uploadRouter from './routes/uploadRoute.js';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
 
 const PORT = process.env.PORT || 3002;
 
@@ -24,3 +26,4 @@ mongoose
   .catch((error) => console.log(error));
 
 app.use('/', userRouter);
+app.use('/upload', uploadRouter);
