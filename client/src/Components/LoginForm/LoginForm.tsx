9ebe0ff6e909af163d9049aef97a4b6c6b81c5fb
Rz0R <vpa05@mail.ru>
2023-02-07 15:22:55 +1000

    feat: add validation for login form

--- client/src/Components/LoginForm/LoginForm.tsx
+++ client/src/Components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useState, MouseEvent } from 'react';
 import { Link } from 'react-router-dom';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 import { APPRoute } from '../../const/const';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
@@ -7,60 +7,66 @@ import { loginAction } from '../../store/serviceActions';
 import { LoginData } from '../../types/userData';
 import './LoginForm.scss';
 
-const initialLoginData: LoginData = {
-  email: '',
-  password: '',
-};
-
 function LoginForm() {
   const dispatch = useAppDispatch();
   const { isLoading } = useAppSelector((state) => state.USER);
 
-  const [formData, setFormData] = useState(initialLoginData);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginData>({
+    mode: 'all',
+  });
 
-  const handleLoginBtnClick = (evt: MouseEvent<HTMLButtonElement>) => {
-    evt.preventDefault();
-    dispatch(loginAction(formData));
+  const handleFormSubmit: SubmitHandler<LoginData> = (data, evt) => {
+    evt?.preventDefault();
+    dispatch(loginAction(data));
   };
-
-  const handleFormChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = evt.currentTarget;
-
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const isButtonDisabled = isLoading || formData.email === '' || formData.password === '';
 
   return (
     <section className="login-form auth-form">
       <div className="login-form__container">
-        <form className="auth-form__wrapper">
+        <form className="auth-form__wrapper" onSubmit={handleSubmit(handleFormSubmit)}>
           <h1 className="auth-form__title">Вход в Trello</h1>
-          <input
-            className="auth-form__input"
-            type="email"
-            placeholder="Укажите адрес электронной почты"
-            required
-            name="email"
-            onChange={handleFormChange}
-          />
-          <input
-            className="auth-form__input"
-            type="password"
-            placeholder="Введите пароль"
-            required
-            name="password"
-            onChange={handleFormChange}
-          />
-          <button
-            className="login-form__btn form-btn"
-            type="submit"
-            onClick={handleLoginBtnClick}
-            disabled={isButtonDisabled}
-          >
+          <label htmlFor="email" className="auth-form__label">
+            <input
+              className="auth-form__input"
+              type="email"
+              placeholder="Укажите адрес электронной почты"
+              {...register('email', {
+                required: 'Поле обязательно к заполнению',
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: 'Значение не соответствует формату эл. почты',
+                },
+              })}
+            />
+            <p className="auth-form__input-error">{errors?.email && errors?.email?.message}</p>
+          </label>
+          <label htmlFor="password" className="auth-form__label">
+            <input
+              className="auth-form__input"
+              type="password"
+              placeholder="Введите пароль"
+              {...register('password', {
+                required: 'Поле обязательно к заполнению',
+                minLength: {
+                  value: 6,
+                  message: 'Минимальная длина пароля 6 символов',
+                },
+                maxLength: {
+                  value: 20,
+                  message: 'Максимальная длина пароля 20 символов',
+                },
+              })}
+            />
+            <p className="auth-form__input-error">
+              {errors?.password && errors?.password?.message}
+            </p>
+          </label>
+
+          <button className="login-form__btn form-btn" type="submit" disabled={isLoading}>
             Войти
           </button>
           <hr className="auth-form__separator" />
