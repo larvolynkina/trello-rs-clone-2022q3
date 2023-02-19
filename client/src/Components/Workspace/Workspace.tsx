f4838df3232d3b9854554738d579444cabd5dc0f
Rz0R <vpa05@mail.ru>
2023-02-19 23:24:08 +1000

    feat: add workspace delete button

--- client/src/Components/Workspace/Workspace.tsx
+++ client/src/Components/Workspace/Workspace.tsx
@@ -1,19 +1,62 @@
+import { toast } from 'react-toastify';
+
 import { IWorkspace } from '../../types/workspace';
 import WorkspaceIcon from './WorkspaceIcon';
 import Boards from './Boards';
-import './Workspace.scss';
 import Title from './Title';
+import { useDeleteWorkspaceMutation } from '../../store/reducers/workspace/workspace.api';
+import { isFetchBaseQueryErrorWithMsg } from '../../utils/error';
+import './Workspace.scss';
 
 type WorkspaceProps = {
   data: IWorkspace;
 };
 
 function Workspace({ data: { title, avatarColor, boards, _id: id } }: WorkspaceProps) {
+  const [deleteWorkspace, { isLoading }] = useDeleteWorkspaceMutation();
+
+  const handleDeleteWorkspace = async () => {
+    const toastId = toast.loading('Удаление рабочего пространства...');
+    try {
+      await deleteWorkspace(id).unwrap();
+      toast.update(toastId, {
+        render: 'Рабочее пространство удалено успешно!',
+        type: 'success',
+        isLoading: false,
+        autoClose: 3000,
+      });
+    } catch (err) {
+      if (isFetchBaseQueryErrorWithMsg(err)) {
+        toast.update(toastId, {
+          render: err.data.message,
+          type: 'error',
+          isLoading: false,
+          autoClose: 3000,
+        });
+      } else {
+        toast.update(toastId, {
+          render: 'Произошла ошибка при удалении рабочего пространства.',
+          type: 'error',
+          isLoading: false,
+          autoClose: 3000,
+        });
+      }
+    }
+  };
+
   return (
     <div className="workspace">
       <div className="workspace__header">
         <WorkspaceIcon title={title} color={avatarColor} />
         <Title title={title} workspaceId={id} />
+        <button
+          type="button"
+          className="workspace__btn"
+          disabled={isLoading}
+          onClick={handleDeleteWorkspace}
+        >
+          Удалить
+        </button>
       </div>
       <Boards data={boards} workspaceId={id} />
     </div>
